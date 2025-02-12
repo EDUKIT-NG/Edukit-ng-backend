15b0ff1c4c6374b4ae8770a8ba46d93e4e8e4f82
Shedrack Okunda <okundashedrack@gmail.com>
2025-02-12 10:49:53 +0300

    Created a utility folder and added in it files to generate token and to sanitize user

---
+++ utils/SanitizeUser.js
@@ -0,0 +1,27 @@
+export const sanitizeUser = (user) => {
+  const sanitizedUser = {
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
+
+  switch (user.role) {
+    case "student":
+      sanitizedUser.isVerified = user.isVerified;
+      sanitizedUser.grade = user.grade;
+      break;
+    case "donor":
+      sanitizedUser.isVerified = user.isVerified;
+      sanitizedUser.donationAmount = user.donationAmount;
+      break;
+    case "admin":
+      sanitizedUser.isVerified = user.isVerified;
+      sanitizedUser.isAdmin = user.isAdmin;
+      break;
+    default:
+      break;
+  }
+
+  return sanitizedUser;
+};
