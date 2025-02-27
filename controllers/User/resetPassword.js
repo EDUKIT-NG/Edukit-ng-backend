a44d8a4848288ac578bf6195d74aef65ea15d38c
“G-Gakii” <gakiiviolet1@gmail.com>
2025-02-27 19:31:43 +0300

    reset password

--- controllers/User/resetPassword.js
+++ controllers/User/resetPassword.js
@@ -1,38 +1,50 @@
-// import School from "../../models/School.js";
-// import bcrypt from "bcrypt";
-// import { generateToken } from "../../utils/GenerateToken.js";
+import expressAsyncHandler from "express-async-handler";
+import bcrypt from "bcrypt";
+import { generateToken } from "../../utils/GenerateToken.js";
+import User from "../../models/User.model.js";
+import mongoose from "mongoose";
+import passwordResetSchema from "../../Validation/User/passwordValidation.js";
 
-// const resetPassword = async (req, res) => {
-//   try {
-//     const { password, schoolId } = req.body;
+const resetPassword = expressAsyncHandler(async (req, res) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    const { id } = req.params;
+    const result = await passwordResetSchema.validateAsync(req.body);
+    const { password } = result;
 
-//     const school = await School.findById(schoolId);
-//     if (!school) {
-//       res.status(404).json({ message: "school not found" });
-//     }
-//     const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.findById(id);
+    if (!user) {
+      await session.abortTransaction();
+      return res.status(404).json({ message: "User not found" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-//     const newSchoolPassword = await School.findByIdAndUpdate(schoolId, {
-//       password: hashedPassword,
-//     });
-//     const secureInfo = { id: school._id, userType: "School" };
+    const newUserPassword = await User.findByIdAndUpdate(id, {
+      password: hashedPassword,
+    });
+    const secureInfo = { id: user._id, userType: user.role };
 
-//     const token = generateToken(secureInfo);
-//     const cookieExpirationDays = parseInt(process.env.COOKIE_EXPIRATION_DAYS);
-//     if (isNaN(cookieExpirationDays)) {
-//       throw new Error("COOKIE_EXPIRATION_DAYS must be a valid number.");
-//     }
-//     // sends JWT token in the response cookies
-//     res.cookie("token", token, {
-//       sameSite: process.env.PRODUCTION === "true" ? "None" : "Lax",
-//       maxAge: cookieExpirationDays * 24 * 60 * 60 * 1000,
-//       httpOnly: true,
-//       secure: process.env.PRODUCTION === "true",
-//     });
-//     return res.status(200).json({ message: "Password reset successfully" });
-//   } catch (error) {
-//     return res.status(500).json({ message: error.message });
-//   }
-// };
+    const token = generateToken(secureInfo);
+    const cookieExpirationDays = parseInt(process.env.COOKIE_EXPIRATION_DAYS);
+    if (isNaN(cookieExpirationDays)) {
+      throw new Error("COOKIE_EXPIRATION_DAYS must be a valid number.");
+    }
 
-// export default resetPassword;
+    res.cookie("token", token, {
+      sameSite: process.env.PRODUCTION === "true" ? "None" : "Lax",
+      maxAge: cookieExpirationDays * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      secure: process.env.PRODUCTION === "true",
+    });
+    await session.commitTransaction();
+    return res.status(200).json({ message: "Password reset successfully" });
+  } catch (error) {
+    await session.abortTransaction();
+    throw error;
+  } finally {
+    session.endSession();
+  }
+});
+
+export default resetPassword;
--- routes/User.js
+++ routes/User.js
@@ -7,19 +7,21 @@ import { Router } from "express";
 // import { EditSchool } from "../controllers/School/editSchool.js";
 // import { deleteSchool } from "../controllers/School/deleteSchool.js";
 // import logoutSchool from "../controllers/School/logout.js";
-// import resetPassword from "../controllers/School/resetPassword.js";
+
 // import { createProfile } from "../controllers/School/SchoolProfile.js";
 import { registerUser } from "../controllers/User/registerUser.js";
 import { verifyOtp } from "../controllers/User/verifyUser.js";
 import resendOtp from "../controllers/User/resendOtp.js";
+import loginUser from "../controllers/User/loginUser.js";
+import resetPassword from "../controllers/User/resetPassword.js";
 
 const router = Router();
 
 router.post("/register", registerUser);
-// router.post("/login", loginSchool);
+router.post("/login", loginUser);
 router.post("/verify/:id", verifyOtp);
 router.post("/resend-otp/:id", resendOtp);
-// router.post("/reset-password", resetPassword);
+router.post("/reset-password/:id", resetPassword);
 // router.get("/", getAllSchools);
 // router.get("/:id", getASingleSchool);
 // router.put("/:id", EditSchool);
