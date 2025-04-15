9ed366cfb4e92d6df0e829811e6afa38c34879f8
sheddy <okundashedrack@gmail.com>
2025-04-15 22:36:15 +0300

    updated the code by adding username, address and phoneNumber when a user tries to create an account and to either use username or email when logging in

--- controllers/User/loginUser.js
+++ controllers/User/loginUser.js
@@ -2,7 +2,6 @@ import expressAsyncHandler from "express-async-handler";
 import { generateToken } from "../../utils/GenerateToken.js";
 import { sanitizeUser } from "../../utils/SanitizeUser.js";
 import User from "../../models/User.model.js";
-
 import bcrypt from "bcrypt";
 import logInSchema from "../../Validation/User/loginValidation.js";
 import mongoose from "mongoose";
@@ -10,10 +9,13 @@ import mongoose from "mongoose";
 const loginUser = expressAsyncHandler(async (req, res) => {
   const session = await mongoose.startSession();
   session.startTransaction();
+
   try {
     const result = await logInSchema.validateAsync(req.body);
-    const { email, password } = result;
-    const existingUser = await User.findOne({ email });
+    const { username, email, password } = result;
+
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+
     if (!existingUser) {
       await session.abortTransaction();
       return res.status(404).json({
@@ -27,6 +29,7 @@ const loginUser = expressAsyncHandler(async (req, res) => {
       res.status(404).json({ message: "User password not found " });
       return;
     }
+
     const isMatch = await bcrypt.compare(password, hashedPassword);
     if (!isMatch) {
       await session.abortTransaction();
@@ -38,18 +41,21 @@ const loginUser = expressAsyncHandler(async (req, res) => {
       await session.abortTransaction();
       return res.status(400).json({ message: "Kindly verify your email" });
     }
+
     const secureInfo = sanitizeUser(existingUser);
     const token = generateToken(secureInfo);
     const cookieExpirationDays = parseInt(process.env.COOKIE_EXPIRATION_DAYS);
     if (isNaN(cookieExpirationDays)) {
       throw new Error("COOKIE_EXPIRATION_DAYS must be a valid number.");
     }
+
     res.cookie("token", token, {
       sameSite: process.env.PRODUCTION === "true" ? "None" : "Lax",
       maxAge: cookieExpirationDays * 24 * 60 * 60 * 1000,
       httpOnly: true,
       secure: process.env.PRODUCTION === "true",
     });
+
     await session.commitTransaction();
     return res.status(200).json({
       message: "Login successful. ",
