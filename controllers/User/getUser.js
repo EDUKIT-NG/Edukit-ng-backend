82be951b639c35e6a7df19609bef44a7d9653649
sheddy <okundashedrack@gmail.com>
2025-04-23 14:43:38 +0300

    Fixed a typo

--- controllers/User/getUser.js
+++ controllers/User/getUser.js
@@ -10,7 +10,7 @@ export const getASingleUser = expressAsyncHandler(async (req, res) => {
   const { id } = req.params;
   let user = await User.findById(id);
   if (!user) {
-    return res.status(404).json({ message: "school not found" });
+    return res.status(404).json({ message: "User not found" });
   }
   res.status(200).json({ user });
 });
