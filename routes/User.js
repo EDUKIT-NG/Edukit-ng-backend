0e8b15936c6ad1c8053d6b3d7c673c128de12aae
sheddy <okundashedrack@gmail.com>
2025-04-24 18:21:04 +0300

    added id to the verify and resend-otp routes

--- routes/User.js
+++ routes/User.js
@@ -1,5 +1,4 @@
 import { Router } from "express";
-
 import { registerUser } from "../controllers/User/registerUser.js";
 import { verifyOtp } from "../controllers/User/verifyUser.js";
 import resendOtp from "../controllers/User/resendOtp.js";
@@ -11,24 +10,24 @@ import {
 import logout from "../controllers/User/logout.js";
 import { getAllUsers, getASingleUser } from "../controllers/User/getUser.js";
 import { deleteUser } from "../controllers/User/deleteUser.js";
-import { createProfileSchool } from "../controllers/User/SchoolProfile.js";
-import createStudentProfile from "../controllers/User/studentProfile.js";
-import { verifyToken } from "../middleware/VerifyToken.js";
+// import { createProfileSchool } from "../controllers/User/SchoolProfile.js";
+// import createStudentProfile from "../controllers/User/studentProfile.js";
+// import { verifyToken } from "../middleware/VerifyToken.js";
 
 const router = Router();
 
 router
   .post("/register", registerUser)
   .post("/login", loginUser)
-  .post("/verify",  verifyOtp)
-  .post("/resend-otp", resendOtp)
+  .post("/verify/:id", verifyOtp)
+  .post("/resend-otp/id", resendOtp)
   .post("/forgot-password", forgotPassword)
   .post("/reset-password", resetPassword)
   .get("/", getAllUsers)
   .get("/:id", getASingleUser)
   .delete("/:id", deleteUser)
-  .post("/logout", logout)
-  .put("/create-school-profile/:id", createProfileSchool)
-  .put("/create-student-profile/:id", createStudentProfile);
+  .post("/logout", logout);
+// .put("/create-school-profile/:id", createProfileSchool)
+// .put("/create-student-profile/:id", createStudentProfile);
 
 export default router;
