bcae492e8c5eb291e4c478d6bc4d278832c0c776
sheddy <okundashedrack@gmail.com>
2025-04-24 13:40:08 +0300

    Added forgot-password route and removed the verify token middleware

--- routes/User.js
+++ routes/User.js
@@ -4,7 +4,10 @@ import { registerUser } from "../controllers/User/registerUser.js";
 import { verifyOtp } from "../controllers/User/verifyUser.js";
 import resendOtp from "../controllers/User/resendOtp.js";
 import loginUser from "../controllers/User/loginUser.js";
-import resetPassword from "../controllers/User/resetPassword.js";
+import {
+  forgotPassword,
+  resetPassword,
+} from "../controllers/User/resetPassword.js";
 import logout from "../controllers/User/logout.js";
 import { getAllUsers, getASingleUser } from "../controllers/User/getUser.js";
 import { deleteUser } from "../controllers/User/deleteUser.js";
@@ -14,17 +17,18 @@ import { verifyToken } from "../middleware/VerifyToken.js";
 
 const router = Router();
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-router.post("/verify", verifyToken, verifyOtp);
-router.post("/resend-otp", verifyToken, resendOtp);
-router.post("/reset-password", verifyToken, resetPassword);
-router.get("/", getAllUsers);
-router.get("/:id", getASingleUser);
-
-router.delete("/:id", deleteUser);
-router.post("/logout", logout);
-router.put("/create-school-profile/:id", createProfileSchool);
-router.put("/create-student-profile/:id", createStudentProfile);
+router
+  .post("/register", registerUser)
+  .post("/login", loginUser)
+  .post("/verify",  verifyOtp)
+  .post("/resend-otp", resendOtp)
+  .post("/forgot-password", forgotPassword)
+  .post("/reset-password", resetPassword)
+  .get("/", getAllUsers)
+  .get("/:id", getASingleUser)
+  .delete("/:id", deleteUser)
+  .post("/logout", logout)
+  .put("/create-school-profile/:id", createProfileSchool)
+  .put("/create-student-profile/:id", createStudentProfile);
 
 export default router;
