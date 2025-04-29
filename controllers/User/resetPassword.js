3d9923c5f250dbb28008a95d896d99268e8b2dcf
sheddy <okundashedrack@gmail.com>
2025-04-29 17:57:06 +0300

    Updated the forgotPassword function to ensure it sends otp instead of link and added new function to verify email

--- controllers/User/resetPassword.js
+++ controllers/User/resetPassword.js
@@ -1,10 +1,9 @@
 import expressAsyncHandler from "express-async-handler";
 import bcrypt from "bcrypt";
-import { generateToken } from "../../utils/GenerateToken.js";
 import User from "../../models/User.model.js";
-import PasswordResetToken from "../../models/PasswordResetToken.js";
-import { sanitizeUser } from "../../utils/SanitizeUser.js";
+import Otp from "../../models/Otp.js";
 import { sendMail } from "../../utils/Email.js";
+import { generateOtp } from "../../utils/GenerateOtp.js";
 
 export const forgotPassword = expressAsyncHandler(async (req, res) => {
   let newToken;
@@ -18,27 +17,24 @@ export const forgotPassword = expressAsyncHandler(async (req, res) => {
     if (!isExistingUser) {
       return res
         .status(404)
-        .json({ message: "Provided email does not exists" });
+        .json({ message: "Provided email does not exists." });
     }
 
-    await PasswordResetToken.deleteMany({ user: isExistingUser._id });
+    await Otp.deleteMany({ user: isExistingUser._id });
 
-    // if user exists, generate a password reset token
-    const passwordResetToken = generateToken(
-      sanitizeUser(isExistingUser),
-      true
-    );
+    // generate a random OTP
+    const otp = generateOtp();
 
-    // hashes the token
-    const hashedToken = await bcrypt.hash(passwordResetToken, 10);
+    // hashes the OTP
+    const hashedOtp = await bcrypt.hash(otp, 10);
 
-    // saves hashed token in passwordResetToken collection
-    newToken = new PasswordResetToken({
+    // saves hashed otp in passwordResetToken collection
+    newToken = new Otp({
       user: {
         id: isExistingUser._id,
         userType: isExistingUser.role,
       },
-      token: hashedToken,
+      otp: hashedOtp,
       expiresAt: Date.now() + parseInt(process.env.OTP_EXPIRATION_TIME),
     });
 
@@ -47,19 +43,13 @@ export const forgotPassword = expressAsyncHandler(async (req, res) => {
     // sends the password reset link to the user's email
     await sendMail(
       isExistingUser.email,
-      "Password reset link",
-      `<p>Dear ${isExistingUser.email},
-                We received a request to reset the password for your account. please use the following link to reset your password:</p>
-                <p><a href=${process.env.ORIGIN}/reset-password/${isExistingUser._id}/${passwordResetToken} target='_blank'>Reset Password</a></p>
-                <p>This link is valid for a limited time. If you did not request a password reset, please ignore this email. Your account security is important to us.
-
-                Thank you,
-                Edukit Team</p>`
+      "Password reset OTP",
+      `Your OTP is: <b>${otp}</b>`
     );
 
     res
       .status(200)
-      .json({ message: `Password reset link sent to ${isExistingUser.email}` });
+      .json({ message: `Password reset OTP sent to ${isExistingUser.email}` });
   } catch (error) {
     res.status(500).json({
       message: "Error occurred while sending password reset email.",
@@ -69,7 +59,8 @@ export const forgotPassword = expressAsyncHandler(async (req, res) => {
 });
 
 export const resetPassword = expressAsyncHandler(async (req, res) => {
-  const { id, token, password } = req.body;
+  const id = req.params.id;
+  const { password } = req.body;
 
   try {
     // checks if user exists or not
@@ -80,39 +71,11 @@ export const resetPassword = expressAsyncHandler(async (req, res) => {
       return res.status(404).json({ message: "User does not exists" });
     }
 
-    // fetches the resetPassword token by the userId
-    const isResetTokenExisting = await PasswordResetToken.findOne({
-      user: isExistingUser._id,
+    // resets the password after hashing it
+    await User.findByIdAndUpdate(isExistingUser._id, {
+      password: await bcrypt.hash(password, 10),
     });
-
-    // if token does not exists for that userId then returns a 404 response
-    if (!isResetTokenExisting) {
-      return res.status(404).json({ message: "Reset link in not valid" });
-    }
-
-    // if the token has expired then deletes the token and send response accordingly
-    if (isResetTokenExisting.expiresAt < new Date()) {
-      await PasswordResetToken.findByIdAndDelete(isResetTokenExisting._id);
-      return res.status(404).json({ message: "Reset link has been expired." });
-    }
-
-    // if token exists and if not expired and token matches the hash, then reset the user password and deletes the token
-    if (
-      isResetTokenExisting &&
-      isResetTokenExisting.expiresAt > new Date() &&
-      (await bcrypt.compare(token, isResetTokenExisting.token))
-    ) {
-      // deleting the password reset token
-      await PasswordResetToken.findByIdAndDelete(isResetTokenExisting._id);
-
-      // resets the password after hashing it
-      await User.findByIdAndUpdate(isExistingUser._id, {
-        password: await bcrypt.hash(password, 10),
-      });
-      return res.status(200).json({ message: "Password updated successfully" });
-    }
-
-    return res.status(404).json({ message: "Reset link has been expired" });
+    return res.status(200).json({ message: "Password updated successfully" });
   } catch (error) {
     res.status(500).json({
       message: "Error occurred while resetting the password, please try again.",
