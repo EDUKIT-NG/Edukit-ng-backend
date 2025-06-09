3c2088c0893bc9979f0528faf14300cdd43fd239
sheddy <okundashedrack@gmail.com>
2025-06-09 17:52:52 +0300

    fix the OAuth signup by separating it from the user.js routes to prevent the dynamic routes from blocking it

---
+++ routes/auth.js
@@ -0,0 +1,21 @@
+// routes/auth.js
+import { Router } from "express";
+import passport from "passport";
+
+const authRouter = Router();
+
+authRouter.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+authRouter.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  (req, res) => {
+    const token = req.user.token;
+    res.redirect(`http://localhost:3000/?token=${token}`);
+  }
+);
+
+export default authRouter;
