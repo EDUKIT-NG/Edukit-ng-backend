0476cf663efa3c60e40459f53362ecbd9e9c6528
Sheddy <okundashedrack@gmail.com>
2025-06-14 14:05:37 +0300

    Intergrated Oauth with frontend for the student

--- passport.js
+++ passport.js
@@ -6,37 +6,38 @@ import dotenv from "dotenv";
 dotenv.config();
 
 passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({
-            googleId: profile.id,
-            email: profile.emails[0].value,
-            name: profile.displayName,
-            password: "google-auth",
-          })
-            .save()
-            .then((user) => done(null, user));
-        }
-      });
-    }
-  )
+	new GoogleStrategy(
+		{
+			clientID: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			callbackURL: "http://localhost:5000/auth/google/callback",
+		},
+		(accessToken, refreshToken, profile, done) => {
+			User.findOne({ googleId: profile.id }).then((existingUser) => {
+				if (existingUser) {
+					done(null, existingUser);
+				} else {
+					new User({
+						googleId: profile.id,
+						email: profile.emails[0].value,
+						name: profile.displayName,
+						password: "google-auth",
+						verified: true,
+					})
+						.save()
+						.then((user) => done(null, user));
+				}
+			});
+		}
+	)
 );
 
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+	done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id).then((user) => {
-    done(null, user);
-  });
+	User.findById(id).then((user) => {
+		done(null, user);
+	});
 });
--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,21 @@
 import { Router } from "express";
 import passport from "passport";
+import { generateAccessToken } from "../utils/GenerateToken.js";
 
 const authRouter = Router();
 
 authRouter.get(
-  "/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
+	"/google",
+	passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
 authRouter.get(
-  "/google/callback",
-  passport.authenticate("google", { failureRedirect: "/login" }),
-  (req, res) => {
-    const token = req.user.token;
-    res.redirect(`http://localhost:3000/?token=${token}`);
-  }
+	"/google/callback",
+	passport.authenticate("google", { failureRedirect: "/login" }),
+	(req, res) => {
+		const token = generateAccessToken(req.user);
+		res.redirect(`http://localhost:3000/?token=${token}`);
+	}
 );
 
 export default authRouter;
